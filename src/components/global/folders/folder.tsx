8b35c8794c7f0fc5700eec506719eeebe4d1b8cc
Ashuvis <ashutoshvishwakarma2478@gmail.com>
2026-07-18 17:12:52 +0530

    reset

--- src/components/global/folders/folder.tsx
+++ src/components/global/folders/folder.tsx
@@ -5,6 +5,8 @@ import React, { useRef, useState } from 'react'
 import Loader from '../loader'
 import FolderDuotone from '@/components/Icons/folder-duotone'
 import { useMutationData } from '@/hooks/useMutationData'
+import { renameFolder } from '@/actions/workspace'
+import { Input } from '@/components/ui/input'
 
 type Props = {
   name: string
@@ -21,7 +23,7 @@ const Folder = ({id, name, optimistic, count}: Props) => {
   const folderCardRef = useRef<HTMLDivElement>(null)
   const Rename=()=>setOnRename(true)
   const Renamed=()=>setOnRename(false)
-  const {mutate,isPending}= useMutationData(['rename-folders'], (data:{name:string})=>renameFolder(), 'workspace-folders',Renamed)
+  const {mutate,isPending}= useMutationData(['rename-folders'], (data:{name:string})=>renameFolder(id,name), 'workspace-folders',Renamed)
 
 
 
@@ -30,14 +32,26 @@ const Folder = ({id, name, optimistic, count}: Props) => {
   }
   const handleNameDoubleclick = (e: React.MouseEvent<HTMLParagraphElement>)=>{
     e.stopPropagation()
+    Rename()
     //rename functionality
+  }
+
+  const updateFolderName = (e: React.FocusEvent<HTMLInputElement>)=>{
+    if(inputRef.current && folderCardRef.current){
+      if(!inputRef.current.contains(e.target as Node | null) && !folderCardRef.current.contains(e.target as Node | null) ){
+        if(inputRef.current.value){
+          mutate({name:inputRef.current.value})
+        }else Renamed()
+      }
+    }
   }
   //add loading states 
     return (
     <div onClick={handleFolderclick} className={cn("flex hover:bg-neutral-800 cursor-pointer transition duration-150 items-center gap-2 justify-between min-w-62.5 py-4 px-4 rounded-lg border")}>
       <Loader state={false} >
         <div className='flex flex-col gap-1'>
-          <p onClick={()=>{handleFolderclick}} onDoubleClick={(e)=>{handleNameDoubleclick}} className='text-[#BDBDBD] '>{name}</p>
+        {onRename?<Input onBlur={(e: React.FocusEvent<HTMLInputElement>)=>{updateFolderName(e)}} autoFocus placeholder={name} className='border-none  text-base w-full outline-none text-neutral-300 bg-transparent p-0' ref={inputRef} />:<p onClick={(e)=>{handleFolderclick}} onDoubleClick={(e)=>{handleNameDoubleclick}} className='text-[#BDBDBD] '>{name}</p>}
+          
           <span className='text-sm text-neutral-500'>{count} videos</span>
         </div>
       </Loader>
