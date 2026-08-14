3d1cf06ebbe34646c6f885be3dc7eefa5d2d45ad
Ashuvis <ashutoshvishwakarma2478@gmail.com>
2026-08-14 22:39:22 +0530

    renaming features revamps

--- src/components/global/folders/folder.tsx
+++ src/components/global/folders/folder.tsx
@@ -1,9 +1,10 @@
 'use client'
-import React from 'react'
-import { usePathname , useRouter} from 'next/navigation'
+import React, { useRef, useState } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import Loader from '../loader'
 import FolderDuotone from '@/components/Icons/folder-duotone'
+import { useMutationData } from '@/hooks/useMutationData'
 
 type Props = {
   name: string
@@ -15,21 +16,38 @@ type Props = {
 const Folder = ({ name, id, optimistic, count }: Props) => {
   const pathname = usePathname()
   const router = useRouter()
+  const [Rename, setRename] = useState(false)
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const foldercardRef = useRef<HTMLDivElement | null>(null)
+  const beforeRename = () => setRename(true)
+  const afterRename = () => setRename(false)
+
+  const handleFolderClick = () => {
+    router.push(`${pathname}/folder/${id}`)
+  }
+  const handleRename = (e: React.MouseEvent<HTMLParagraphElement>) => {
+    e.stopPropagation()
+    //rename function
+  }
+  //Some optimistic UI stuff
+  const { mutate, isPending } = useMutationData(['rename-folders'], (data: { name: string }) => renameFolders(), 'workspace-folders', afterRename)
+
+
   //WIP: add LOADING states
   return (
-    <div className={cn('flex hover:bg-neutral-800 cursor-pointer transition-colors duration-150 items-center gap-2 justify-between min-w-[250px] py-6 px-4 rounded-lg border-[1px]')}>
+    <div onClick={handleFolderClick} className={cn('flex hover:bg-neutral-800 cursor-pointer transition-colors duration-150 items-center gap-2 justify-between min-w-[250px] py-6 px-4 rounded-lg border-[1px]')}>
 
       <Loader state={false}>
         <div className='flex flex-col gap-2'>
-          <p className='text-neutral-300 '>
+          <p onDoubleClick={(e) => { handleRename }} className='text-neutral-300 '>
             {name}
           </p>
           <span className='text-sm text-neutral-500 bg-neutral-500'>
-            {count||0}Videos
+            {count || 0}Videos
           </span>
         </div>
       </Loader>
-      <FolderDuotone/>
+      <FolderDuotone />
     </div>
   )
 }
